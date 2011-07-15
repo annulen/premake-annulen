a30ef1b72fbcd4952948de7b3ab95bb328579140
Konstantin Tokarev <ktokarev@smartlabs.tv>
2011-07-15 12:54:48 +0400

    Added debug into Lua

--- src/host/lua-5.1.4/src/lcode.c
+++ src/host/lua-5.1.4/src/lcode.c
@@ -6,6 +6,7 @@
 
 
 #include <stdlib.h>
+#include <stdio.h>
 
 #define lcode_c
 #define LUA_CORE
@@ -81,8 +82,10 @@ static void fixjump (FuncState *fs, int pc, int dest) {
   Instruction *jmp = &fs->f->code[pc];
   int offset = dest-(pc+1);
   lua_assert(dest != NO_JUMP);
-  if (abs(offset) > MAXARG_sBx)
+  if (abs(offset) > MAXARG_sBx) {
+    printf("Debug: offset=%d, MAXARG_sBx=%d", offset, MAXARG_sBx);
     luaX_syntaxerror(fs->ls, "control structure too long");
+  }
   SETARG_sBx(*jmp, offset);
 }
 
